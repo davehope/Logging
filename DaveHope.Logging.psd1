daf9b52f222b3217cf51b3b01d43bde20702900b
Dave Hope <dave@hope.mx>
2020-02-22 08:39:12 +0000

    Initial commit

---
+++ DaveHope.Logging.psd1
@@ -0,0 +1,17 @@
+@{
+    RootModule = 'DaveHope.Logging.psm1'
+    ModuleVersion = '1.1.1'
+    GUID = 'e2817eb3-69a0-45b0-83a6-f12f3853c480'
+    Author = 'Dave Hope'
+    Copyright = '(c) 2020 Dave Hope. All Rights Reserved.'
+    Description = 'PowerShell module to write log messages with a single call'
+    PowerShellVersion = '4.0'
+    RequiredModules = @()
+    FunctionsToExport = 'Write-LogMessage'
+    PrivateData = @{
+        PSData = @{
+            Tags = @('Logging')
+            ProjectUri = 'https://github.com/davehope/Logging'
+        }
+    }
+}
---
+++ DaveHope.Logging.psm1
@@ -0,0 +1,122 @@
+Function Get-LoggingPath
+{
+	[cmdletbinding()]
+	<#
+	.SYNOPSIS
+	Gets the path to the file at the top of the call stack and uses it to build
+	a full path to the directory in which logs should be stored.
+	#>
+	$CallStack = @(Get-PSCallStack) | Select-Object -Property *
+	if($null -ne $CallStack[-1].ScriptName)
+	{
+		$logBaseDir = Split-Path ($CallStack[-1].ScriptName) -Parent
+		$logChildDir = [system.io.path]::GetFilenameWithoutExtension((Split-Path ($CallStack[-1].ScriptName) -Leaf))
+	}
+	else
+	{
+		$scriptPath = ($CallStack |Where-Object{$null -ne $_.ScriptName} | Select-Object -Last 1)[0].ScriptName
+		$logBaseDir = Split-Path $scriptPath -Parent
+		$logChildDir = [system.io.path]::GetFilenameWithoutExtension($scriptPath)
+	}
+
+	$res = $logBaseDir + [IO.Path]::DirectorySeparatorChar `
+		+ "Logs" + [IO.Path]::DirectorySeparatorChar `
+		+ $logChildDir + [IO.Path]::DirectorySeparatorChar
+
+	Write-Verbose "Logging will write to $res"
+	$res
+}
+
+
+Function Write-LogMessage
+{
+	<#
+	.SYNOPSIS
+	Appends a line to a log file, creates the directory structure required if it
+	does not already exist.
+
+	.PARAMETER msg
+	The message to be written to the log file
+
+	.PARAMETER Directory
+	The directory in which to place log files. Messages will be written to a file
+	based on the date/time of the first log message for this execution. If not
+	set, this will be determined automatically based on the location of the
+	calling script
+
+	.PARAMETER exception
+	If an exception is passed, in addition to the message passed a friendly
+	representation of the exception will be included in the log message.
+
+	.PARAMETER quiet
+	If the quiet parameter is used, the message will be echoed as verbose rather
+	than as normal output.
+	#>
+	[cmdletbinding()]
+	Param(
+		[Parameter(Mandatory=$true,Position=0)][string]$msg,
+		[Parameter()][string]$Directory,
+		[Parameter()][System.Exception]$exception,
+		[Parameter()][bool]$quiet=$true
+	)
+
+
+	# Establish the path we will log to. If a global variable exists this will
+	# be used so that subsequent calls during execution don't go to a different
+	# file.
+	$dateTime = [string] (get-date -format yyyy.MM.dd-HHmm)
+	if($null -eq $script:DaveHopeLoggingPath)
+	{
+		if(-not $Directory)
+		{
+			$Directory = Get-LoggingPath
+		}
+		else
+		{
+			if(-not $Directory.EndsWith([IO.Path]::DirectorySeparatorChar))
+			{
+				$Directory += [IO.Path]::DirectorySeparatorChar
+			}
+		}
+		$logPath = $Directory + "$($dateTime).txt"
+		$script:DaveHopeLoggingPath = $logPath
+	}
+	else
+	{
+		$logPath = $script:DaveHopeLoggingPath
+	}
+
+	# Check that our log path exists, including all sub-directories.
+	$dir = Split-Path $logPath
+	$dt = [string] (get-date -format yyyy.MM.dd-HHmm)
+	$logMsg = "[$dt] $msg"
+
+	if(-not (Test-Path $dir))
+	{
+		New-item $dir -ItemType Directory | Out-Null
+	}
+	if(-not (Test-Path $logPath))
+	{
+		New-item $logPath -ItemType File | Out-Null
+	}
+
+	# If an exception has been given, dump that too.
+	if($null -ne $exception)
+	{
+		$logMsg += "`r`n" +
+			"`tCaught an exception:`r`n" +
+			"`tException Type: $($exception.GetType().FullName)`r`n" +
+			"`tException Message: $($exception.Message)"
+	}
+
+	# Actually write the log entry.
+	Add-Content $logPath $logMsg
+	if($quiet)
+	{
+		Write-Verbose $logMsg
+	}
+	else
+	{
+		Write-Output $logMsg
+	}
+}
